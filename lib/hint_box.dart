8ff57c3f79e007537516c4f42492106eb4b2db94
Bader-Al <bader.aalami@gmail.com>
2022-03-18 20:08:39 +0300

    temp dependencies

--- lib/hint_box.dart
+++ lib/hint_box.dart
@@ -2,6 +2,7 @@ library hint_box;
 
 import 'dart:math';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
